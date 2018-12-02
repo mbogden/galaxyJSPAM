8c112d364c4c8f0ebbcb6cec8137b0e2766b6dd4
Jackson L. Cole <jackson.lane.cole@gmail.com>
2018-12-02 16:13:00 -0600

    Renamed merger_class.py -> merger.py

--- python/jspamcli/src/merger/__init__.py
+++ python/jspamcli/src/merger/__init__.py
@@ -9,7 +9,7 @@ __all__ = ['MergerRun',
            'data_tools',
            'get_target_data']
 
-from merger_class import MergerRun
+from merger import MergerRun
 from galaxy import Galaxy
 from get_target_data import get_target_data
 import data_tools
