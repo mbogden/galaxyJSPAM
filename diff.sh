ca2760b1bb408e2bbc54e3fb737442f92929abc1
Matthew Ogden <ogdenm12@gmail.com>
2019-03-15 14:20:15 -0500

    Adding misc scripts I am working with

--- diff.sh
+++ diff.sh
@@ -1,8 +1,8 @@
 # Simple 
 
 g++ -I cpp/include -c cpp/src/imgClass.cpp -o cpp/build/imgClass.o `pkg-config --cflags --libs opencv`
-g++ -I cpp/include -c cpp/src/diff.cpp -o cpp/build/diff.o `pkg-config --cflags --libs opencv`
-g++ -I cpp/include cpp/build/diff.o cpp/build/imgClass.o -o cpp/bin/diff.out `pkg-config --cflags --libs opencv`
+g++ -I cpp/include -c cpp/src/diffAll.cpp -o cpp/build/diffAll.o `pkg-config --cflags --libs opencv`
+g++ -I cpp/include cpp/build/diffAll.o cpp/build/imgClass.o -o cpp/bin/diffAll.out `pkg-config --cflags --libs opencv`
 
 
-./cpp/bin/diff.out output/588017702948962343/ targets/588017702948962343/588017702948962343.calibrated.png targets/588017702948962343/588017702948962343.info.txt
+./cpp/bin/diffAll.out output/588017702948962343/ targets/588017702948962343/588017702948962343.calibrated.png targets/588017702948962343/588017702948962343.info.txt
--- img.sh
+++ img.sh
@@ -3,5 +3,5 @@
 g++ -Wall -g -std=c++17 `pkg-config --cflags --libs opencv` -I cpp/include -c cpp/src/imgClass.cpp -o cpp/build/imgClass.o
 g++ -Wall -g -std=c++17 `pkg-config --cflags --libs opencv` -fopenmp -I cpp/include -c cpp/src/imgCreator.cpp -o cpp/build/imgCreator.o
 g++ -Wall -g -std=c++17 `pkg-config --cflags --libs opencv` -fopenmp -I cpp/include -c cpp/src/img.cpp -o cpp/build/img.o
-g++ -Wall -g -std=c++17 `pkg-config --cflags --libs opencv` -fopenmp  -I cpp/include cpp/build/imgClass.o cpp/build/imgCreator.o cpp/build/img.o -o cpp/bin/img
+g++ -Wall -g -std=c++17 `pkg-config --cflags --libs opencv` -fopenmp  -I cpp/include cpp/build/imgClass.o cpp/build/imgCreator.o cpp/build/img.o -o cpp/bin/img.out
 ./cpp/bin/img output/588017702948962343/run0000/ param0202.txt
