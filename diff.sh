9882f5eb829a344afbef5c677b4da7b0951e0d8f
Matthew Ogden <ogdenm12@gmail.com>
2019-03-22 09:59:57 -0500

    Renaming new quick script for difference code

--- diff.sh
+++ diff.sh
@@ -1,8 +1,11 @@
 # Simple 
 
-g++ -I cpp/include -c cpp/src/imgClass.cpp -o cpp/build/imgClass.o `pkg-config --cflags --libs opencv`
-g++ -I cpp/include -c cpp/src/diffAll.cpp -o cpp/build/diffAll.o `pkg-config --cflags --libs opencv`
-g++ -I cpp/include cpp/build/diffAll.o cpp/build/imgClass.o -o cpp/bin/diffAll.out `pkg-config --cflags --libs opencv`
+g++ -I cpp/include -c Comparison_Methods/Direct_Comparison/src/imgClass.cpp -o Comparison_Methods/Direct_Comparison/build/imgClass.o `pkg-config --cflags --libs opencv`
+
+g++ -I cpp/include -c Comparison_Methods/Direct_Comparison/src/diffAll.cpp -o Comparison_Methods/Direct_Comparison/build/diffAll.o `pkg-config --cflags --libs opencv`
+
+g++ -I cpp/include Comparison_Methods/Direct_Comparison/build/diffAll.o Comparison_Methods/Direct_Comparison/build/imgClass.o -o Comparison_Methods/Direct_Comparison/bin/diffAll.out `pkg-config --cflags --libs opencv`
 
 
-./cpp/bin/diffAll.out output/588017702948962343/ targets/588017702948962343/588017702948962343.calibrated.png targets/588017702948962343/588017702948962343.info.txt
+./Comparison_Methods/Direct_Comparison/bin/diffAll.out output/588017702948962343/ targets/588017702948962343/588017702948962343.calibrated.png targets/588017702948962343/588017702948962343.info.txt
+
