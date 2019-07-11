bc3157baf421e093f5fc094899ad8e05db582c68
Matthew Ogden <ogdenm12@gmail.com>
2019-07-11 15:14:51 -0500

    Modifying img.cpp to be more flexible

--- Image_Creator/cpp/src/img.cpp
+++ Image_Creator/cpp/src/img.cpp
@@ -1,7 +1,8 @@
 // Author: 		Matthew Ogden
-// Created: 	09 July 2019
-// Altered: 	09 July 2019
-// Description: 	This is my main program for creating images out of particle files
+// Created: 	Fall 2018
+// Altered: 	11 July 2019
+// About: 		This program is my main program for reading particle files
+// 				Created by the SPAM program by Dr. Wallin
 
 
 #include "imgCreator.hpp"
@@ -9,17 +10,51 @@
 using namespace cv;
 using namespace std;
 
-
-
 //  In command line.
 //  ./image_creator.exe    run_directory    param_file
 int main(int argc, char *argv[]){
 
-	bool overWriteImages = true;
+  
+	bool overWriteImages = false;
 	bool printStdWarnings = true;
-	string sdss_directory;
+
+	string p1Loc = "", p2Loc = "";
+	string saveImageLoc = "";
+
+	// Read arguments
+	printf("Found %d arguments\n",argc);
+
+	for( int i=0; i<argc; i++){
+
+	  //printf("Arg %d: %s\n",i,argv[i]);
+
+	  if ( string( argv[i] ) == "-overwrite" ){
+		printf("overwrite true\n");
+		overWriteImages = true;
+	  }
+
+	  if ( string( argv[i] ) == "-p1" ){
+		p1Loc = string(argv[i+1]);
+		printf("Using initial particle file %s\n",p1Loc.c_str());
+	  }
+
+	  if ( string( argv[i] ) == "-p2" ){
+		p2Loc = string(argv[i+1]);
+		printf("Using final particle file %s\n",p2Loc.c_str());
+	  }
+
+	  if ( string( argv[i] ) == "-o" ){
+		saveImageLoc = string(argv[i+1]);
+		printf("Saving image to %s\n",saveImageLoc.c_str());
+	  }
+
+	}
+
+
+	return 0;
 
     cout << endl;
+	cout << "\nIn img.cpp" << endl;
     //  Get main Directory
     string mainPath = argv[1];
 
@@ -28,29 +63,31 @@ int main(int argc, char *argv[]){
     if (temp != "/")
         mainPath = mainPath + '/';
 
-
     // Open parameter file and save name
     string tempParam = argv[2];
-
 	ifstream paramfile(tempParam.c_str());
     if (paramfile.fail())
-        printf("Parameter file not found.\nExiting...\n");
-		return -1;
+        printf("Parameter file not found.  Using default param0001\n");
 
 	paramStruct param;
 	param.loadParam(paramfile);
     param.print();
-
 	paramfile.close();
 
 
     printf("About to create image!\n");
   
 	ImgCreator myCreator(mainPath, param, overWriteImages, printStdWarnings);			
+	printf("Constructor check\n");
+
 	if( myCreator.prepare() ){
-		myCreator.makeImage2();
+		printf("prepare check\n");
+		myCreator.makeImage2("testName.png");
+		printf("make image check\n");
 		myCreator.writeInfo();
-		myCreator.delMem();
+		printf("write info check\n");
+	  	myCreator.delMem();
+		printf("delete memory check\n");
 	}	
 	else
 	  printf("In img.cpp.  Creating image for %s failed.\n",mainPath.c_str());
