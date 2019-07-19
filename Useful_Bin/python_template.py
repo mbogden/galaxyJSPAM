887c221acdaf94b4af15f53e2eefca0e28a6c4f0
Matthew Ogden <ogdenm12@gmail.com>
2019-07-19 10:10:12 +0200

    Adding a python template for how I've been structuring my python programs

---
+++ Useful_Bin/python_template.py
@@ -0,0 +1,75 @@
+'''
+    Author:     Matthew Ogden
+    Created:    19 July 2019
+    Altered:    
+Description:    This is my python template for how I've been making my python programs
+'''
+
+from sys import \
+        exit, \
+        argv
+
+
+printAll = True
+
+
+def main():
+
+    endEarly = readArg()
+
+    if endEarly:
+        exit(-1)
+
+# End main
+
+def readArg():
+
+    global printAll
+
+    argList = argv
+
+    for i,arg in argList:
+
+        if arg[0] != '-':
+            continue
+
+        elif arg == '-argFile':
+            argFileLoc = argList[i+1]
+            argList = readArgFile( argList, argFileLoc ) 
+
+        elif arg == '-noprint':
+            printAll = False
+
+    # Check if input arguments were valid
+    endEarly = False
+
+# End reading command line arguments
+
+
+def readArgFile(argList, argFileLoc):
+
+    try:
+        argFile = open( argFileLoc, 'r')
+    except:
+        print("Failed to open/read argument file '%s'" % argFileLoc)
+    else:
+
+        for l in argFile:
+            l = l.strip()
+
+            # Skip line if comment
+            if l[0] == '#':
+                continue
+
+            # Skip line if empty
+            if len(l) == 0:
+                continue
+
+            lineItems = l.split()
+            for item in lineItems:
+                argList.append(item)
+        # End going through file
+# end read argument file
+
+# Run main after declaring functions
+main()
