eccf4f688ca3f55458170698f996962947f35491
Matthew Ogden <ogdenm12@gmail.com>
2019-07-31 10:00:23 -0500

    Commiting misc change. 20190731

---
+++ cSpam.sh
@@ -0,0 +1,7 @@
+
+# Create list of executable commands
+python3 SPAM/python/cmd_zooRun.py \
+  -o Input_Data/spam_input/cmd_spam_cluster_all.txt \
+  -zooLoc Input_Data/spam_input/zoo_all_models.txt \
+  -nPart 100000 \
+  -dataDirName /home/mbo2d/localstorage/mbo2d/spam_data/ \
--- spam.sh
+++ spam.sh
@@ -3,8 +3,8 @@
 python3 SPAM/python/cmd_zooRun.py \
   -o Input_Data/spam_input/cmd_spam_test.txt \
   -zooLoc Input_Data/spam_input/zoo_all_models.txt \
-  -nPart 10000 \
-  -n 5
+  -nPart 1000 \
+  -n 1
 
 # Test executable commands
 python3 Useful_Bin/batch_execution.py \
