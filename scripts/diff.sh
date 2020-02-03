a7ee945fa6fd5c8fc52e238c193de6796936c4b0
Matthew Ogden <ogdenm12@gmail.com>
2020-02-03 13:01:28 -0600

    misc script

--- scripts/diff.sh
+++
@@ -1,2 +0,0 @@
-g++ -ggdb difference_code/diff.cpp difference_code/imgClass.cpp -o diff.out `pkg-config --cflags --libs opencv`
-#./diff.out output/587722984435351614/ targets/587722984435351614/587722984435351614.calibrated.png targets/587722984435351614/587722984435351614.info.txt
