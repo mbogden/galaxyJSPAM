b7fda6bb0aac7cfab35e4fb280e40c5cf94a98ef
Leo_station <ogdenm12@gmail.com>
2021-02-11 20:01:38 -0600

    Cleaning up old and unused code. Part 2

--- Simulator/cluster/prep.sh
+++
@@ -1,5 +0,0 @@
-# This script is for convenience to compile mpi job and begin server
-cd galaxyJSPAM && git pull
-cd ~
-mpicc -o mpi_test galaxyJSPAM/Useful_Bin/cluster/mpiMasterWorker.c -lm
-qsub galaxyJSPAM/Useful_Bin/cluster/qSPAM.qsub
